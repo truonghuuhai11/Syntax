1e72c465aa3b8fc34e4f098eebe48dbf56160aac
Trương Hữu Hải <144077948+truonghuuhai11@users.noreply.github.com>
2023-12-16 07:39:38 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,32 @@
+package basic.dev;
+
+/**
+ * Ghi chuì (comment nhiêÌ u doÌ ng)
+ * 
+ * @author TranThanhBinh 1. Tên lõìp truÌng tên file 2. Tên lõìp chıŞ caìi ğâÌu
+ *         viêìt hoa. Nêìu tıÌ gheìp thiÌ nhıŞng chıŞ caìi dâÌu viêì t hoa 4.
+ *         Tên lõìp ğãò t phaÒ i coì yì nghiŞ a. Viì duò : class Peson : lõì p
+ *         mô taÒ con ngıõÌ i - Interface: PhaÒ i coì tiêÌ n tôì I. Viì duò :
+ *         IListener - Abstract: PhaÒ i coì tiêÌ n tôì Abstract. Viì duò :
+ *         AbstractShape
+ */
+public class MainApp {
+//Comment 1 doÌng caìch 1. Phím noì ng Ctr + /
+	/* Comment 1 doÌ ng caìch 2 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		/**
+		 * Caìch ğãòt tên biêìn: 1. Không chıìa kyì tıò ğãòc biêòt !@#$%^&*()-+= 2.
+		 * Không chıìa khoaÒng trãìng 3. Viêìt thıõÌng 4. Không ğıõòc bãìt ğâÌ u bãÌng
+		 * sôì
+		 */
+		int number;
+		String nameSchool;
+	}
+
+	/*
+	 * tên phıõng thıìc tuân theo quy tãìc camelCase
+	 */
+	public void show() {
+	}
+}
